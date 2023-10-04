edff0766ca6bbf4713a488405ddc3d6f2e729a59
RP19981998 <r4peng@ucsd.edu>
2023-10-04 16:11:07 -0700

    hw3 complete

---
+++ language_model.py
@@ -0,0 +1,40 @@
+import numpy as np
+from segtok import tokenizer
+import torch as th
+from torch import nn
+
+# Using a basic RNN/LSTM for Language modeling
+class LanguageModel(nn.Module):
+    def __init__(self, vocab_size, rnn_size, num_layers=1, dropout=0):
+        super().__init__()
+
+        # Create an embedding layer of shape [vocab_size, rnn_size]
+        # Use nn.Embedding
+        # That will map each word in our vocab into a vector of rnn_size size.
+        self.embedding = nn.Embedding(vocab_size,rnn_size)
+
+        # Create an LSTM layer of rnn_size size. Use any features you wish.
+        # We will be using batch_first convention这个参数指定了输入数据的维度顺序
+        #(batch_size, sequence_length, input_size/rnn_size)
+        self.lstm = nn.LSTM(input_size=rnn_size,hidden_size=rnn_size,num_layers=num_layers,batch_first=True)
+        
+        if dropout!=0:
+          self.drop=True
+          self.dropout=nn.Dropout(p=dropout)
+        elif dropout==0:
+          self.drop=False
+           
+        # LSTM layer does not add dropout to the last hidden output.
+        # Add this if you wish.
+        # self.dropout = nn.Dropout(p=dropout)
+        # Use a dense layer to project the outputs of the RNN cell into logits of
+        # the size of vocabulary (vocab_size).
+        self.output = nn.Linear(rnn_size, vocab_size)
+
+    def forward(self,x):
+        embeds = self.embedding(x)
+        lstm_out, _ =self.lstm(embeds)
+        if self.drop==True:
+          lstm_out=self.dropout(lstm_out)
+        logits = self.output(lstm_out)
+        return logits
